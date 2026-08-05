ca9d903e51e8d37bd5dce3ea2948fd4ae25614bf
Harnoor Kaur <harnoor6ninthbhvm@gmail.com>
2026-08-05 22:50:34 +0530

    Day 1 : Variables, Dtypes & Operators

---
+++ Phase 2/Week 1/script.js
@@ -0,0 +1,26 @@
+// Day 1 - Variables, Data Types, and Operators
+
+// Variables
+const name = "Harnoor";
+let age = 24;
+
+console.log(typeof(name));
+console.log(typeof(age)); 
+
+age += 1;
+console.log(age); 
+
+// name = "Someone" // can't reassign
+
+// All Data Types
+const isStudent = true;
+console.log(typeof(isStudent)); // boolean
+
+// Exercise
+let x = 10;
+const a = "Tanveer";
+const is = false;
+
+console.log(typeof(x));
+console.log(typeof(a));
+console.log(typeof(is));
